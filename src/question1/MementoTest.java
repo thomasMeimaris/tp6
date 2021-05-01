df2fccfebbc7c1b5dd85fde3033da5362ddb0347
thomasMeimaris <thomas.meimaris@gmail.com>
2021-05-01 19:27:54 +0200

    fix class Test

--- src/question1/MementoTest.java
+++ src/question1/MementoTest.java
@@ -2,7 +2,7 @@ package question1;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MementoTest {
     @Test
